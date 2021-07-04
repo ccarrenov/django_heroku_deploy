a8c63368828da3b4496f837c68b07566a88ad425
Cristian Carreño <cristian_c_v@hotmail.com>
2021-07-04 00:13:49 -0400

    heroku config

--- django_heroku_deploy/urls.py
+++ django_heroku_deploy/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_heroku_deploy.views.home import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+    path('', home),
+]
