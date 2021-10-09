cfc372c0dd3dc2166fa17e93e511936ac6fd3631
Cristian Carreño <cristian_c_v@hotmail.com>
2021-10-09 18:04:34 -0300

    update project

--- django_heroku_deploy/views/home.py
+++ django_heroku_deploy/views/home.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse, HttpRequest
 
 def home(request):
-    return HttpResponse('HOME PAGE.')
+    #return HttpResponse('HOME PAGE.')
+    print('home')
+    return render(request, 'index.html')
