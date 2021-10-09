fe16313ce64e0bd4e17661ccac2acdfdce8e9b2d
Cristian Carreño <cristian_c_v@hotmail.com>
2021-10-09 19:28:42 -0300

    update project

--- django_heroku_deploy/wsgi.py
+++ django_heroku_deploy/wsgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 prod_settings = os.getenv('PROD_SETTINGS')
+print('PROD_SETTINGS: '.format(prod_settings))
 
 if not prod_settings and prod_settings == 'True':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_heroku_deploy.settings')
